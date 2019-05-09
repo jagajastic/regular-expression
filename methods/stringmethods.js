7bae5b42c7589af26001d6223187d2c29d82e98a
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-09 16:28:34 +0100

    Add toUpper method: this convert string to upper case

--- methods/stringmethods.js
+++ methods/stringmethods.js
@@ -3,6 +3,19 @@ String.prototype.hasVowels = function () {
     let regexPattern = /[aeiou]/, resultOfCheck = regexPattern.test(this);
     return resultOfCheck;
 }
+
+// toUpper, function to convert string to upper case
+String.prototype.toUpper = function () {
+   var caseString = "";
+   for (i = 0; i < this.length; i++) {
+       if (this.charCodeAt(i) > 96 && this.charCodeAt(i) < 123) {
+         caseString += String.fromCharCode(this.charCodeAt(i) - 32);
+       } else {
+         caseString += String.fromCharCode(this.charCodeAt(i));
+       }
+   }
+   return caseString;
+ }
 //exporting String prototype for method to be access
 module.exports = String.prototype;
 
