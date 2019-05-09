b8bb99ad92ceb18150e9380909b5398aa5d415bc
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-09 20:40:50 +0100

    Add ucFirst method, convert first character in a string to uppercase

--- methods/stringmethods.js
+++ methods/stringmethods.js
@@ -9,25 +9,35 @@ String.prototype.toUpper = function () {
     var caseString = "";
     for (i = 0; i < this.length; i++) {
         if (this.charCodeAt(i) > 96 && this.charCodeAt(i) < 123) {
-          caseString += String.fromCharCode(this.charCodeAt(i) - 32);
-          continue;
+            caseString += String.fromCharCode(this.charCodeAt(i) - 32);
+            continue;
         }
         caseString += String.fromCharCode(this.charCodeAt(i));
     }
     return caseString;
- }
+}
 
 //  toLower, function to conveert upper case string to lowercase
 String.prototype.toLower = function () {
     var caseString = "";
     for (index = 0; index < this.length; index++) {
-      if (this.charCodeAt(index) > 64 && this.charCodeAt(index) < 91) {
-        caseString += String.fromCharCode(this.charCodeAt(index) + 32);
-        continue;
-      }
-      caseString += String.fromCharCode(this.charCodeAt(index));
-  }
-  return caseString;
+        if (this.charCodeAt(index) > 64 && this.charCodeAt(index) < 91) {
+            caseString += String.fromCharCode(this.charCodeAt(index) + 32);
+            continue;
+        }
+        caseString += String.fromCharCode(this.charCodeAt(index));
+    }
+    return caseString;
+}
+
+// ucFirst, function to convert first character of a string to uppercase
+String.prototype.ucFirst = function () {
+    let regexPattern = /^[a-z]/, resultOfCheck = regexPattern.test(this);
+    let remainChar = this.slice(1);
+    if (resultOfCheck) {
+        return (this[0].toUpper()) + remainChar;
+    }
+    return (this[0] + remainChar);
 }
 //exporting String prototype for method to be access
 module.exports = String.prototype;
